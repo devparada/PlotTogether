a17072ced958cdb3f19b545650f0ebabbf9148b8
Raúl <raul.parada040103@gmail.com>
2024-09-30 22:06:41 +0200

    Varias mejoras

---
+++ player.gd
@@ -0,0 +1,37 @@
+extends CharacterBody3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+@onready var camera_3d: Camera3D = $Camera3D
+
+# Se captura el ratón para la camara
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+# Rota la camara con el ratón
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * 0.001)
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("press_left", "press_right", "press_up", "press_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
